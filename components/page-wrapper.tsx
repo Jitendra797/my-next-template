a71ac953de15cee8db42341f34a232f762306c13
Charan <charanxmn@gmail.com>
2024-11-02 02:54:10 +0530

    Modified the top bar in the page wrapper.
    - Added a profile button with a placeholder DropdownMenu.
    - Added the dark mode toggle.

--- components/page-wrapper.tsx
+++ components/page-wrapper.tsx
@@ -17,6 +17,17 @@ import {
 } from "@/components/ui/sidebar";
 import { usePathname } from "next/navigation";
 import React from "react";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { ModeToggle } from "./mode-toggle";
+import Link from "next/link";
 
 export default function PageWrapper({
   children,
@@ -34,42 +45,66 @@ export default function PageWrapper({
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              {/* Breadcrumb list is created dynamically using the current path name */}
-              {paths.map((path, index) => {
-                if (index == paths.length - 1) {
+        <header className="flex h-16 shrink-0 items-center justify-between border-b px-4">
+          <div className="flex items-center gap-2">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                {/* Breadcrumb list is created dynamically using the current path name */}
+                {paths.map((path, index) => {
+                  if (index == paths.length - 1) {
+                    return (
+                      <BreadcrumbItem key={path} className="hidden md:block">
+                        <BreadcrumbPage className="capitalize">
+                          {getNameForPath(path)}
+                        </BreadcrumbPage>
+                      </BreadcrumbItem>
+                    );
+                  }
                   return (
-                    <BreadcrumbItem key={path} className="hidden md:block">
-                      <BreadcrumbPage className="capitalize">
-                        {getNameForPath(path)}
-                      </BreadcrumbPage>
-                    </BreadcrumbItem>
+                    <>
+                      <BreadcrumbItem key={path} className="hidden md:block">
+                        <BreadcrumbLink
+                          href={`/${paths.slice(0, index + 1).join("/")}`}
+                          className="capitalize"
+                        >
+                          {getNameForPath(path)}
+                        </BreadcrumbLink>
+                      </BreadcrumbItem>
+                      {index != 0 || index != paths.length - 1 ? (
+                        <BreadcrumbSeparator />
+                      ) : (
+                        <></>
+                      )}
+                    </>
                   );
-                }
-                return (
-                  <>
-                    <BreadcrumbItem key={path} className="hidden md:block">
-                      <BreadcrumbLink
-                        href={`/${paths.slice(0, index + 1).join("/")}`}
-                        className="capitalize"
-                      >
-                        {getNameForPath(path)}
-                      </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    {index != 0 || index != paths.length - 1 ? (
-                      <BreadcrumbSeparator />
-                    ) : (
-                      <></>
-                    )}
-                  </>
-                );
-              })}
-            </BreadcrumbList>
-          </Breadcrumb>
+                })}
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          <div className="flex items-center gap-2">
+            <ModeToggle />
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon" className="rounded-full">
+                  CN
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Settings</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem asChild>
+                    <Link href="/">Logout</Link>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
       </SidebarInset>
