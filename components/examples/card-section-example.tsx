452d73ae718554b7ec15f1731129ab0c22da1582
Charan <charanxmn@gmail.com>
2025-02-02 16:18:52 +0530

    Refactor CardSectionExample to remove CardsViewSection and directly render ExampleCard components

--- components/examples/card-section-example.tsx
+++ components/examples/card-section-example.tsx
@@ -1,12 +1,13 @@
-import CardsViewSection from "../sections/cards-view-section";
 import ExampleCard from "./example-card";
 
 export default function CardSectionExample() {
   return (
-    <CardsViewSection sectionName="Card Section Example">
-      {[1, 2, 3, 4, 5].map((i) => (
-        <ExampleCard key={i} />
-      ))}
-    </CardsViewSection>
+    <>
+      {
+        [1, 2, 3, 4, 5].map((i) => (
+          <ExampleCard key={i} />
+        ))
+      }
+    </>
   );
 }
