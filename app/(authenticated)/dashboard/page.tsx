c5d7bc524a9ef24b53723e555e4df650540d7ac3
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-03-10 23:54:20 +0530

    Refactor dashboard page to use ContentLayout component and simplify structure

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -1,28 +1,13 @@
-import { ExampleAreaChart } from "@/components/examples/area-chart";
-import { NewWorkflowForm } from "@/components/examples/new-workflow-form";
-import PageSection from "@/components/page-section";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { ContentLayout } from "@/components/admin-panel/content-layout";
 
 const Page = () => {
   return (
-    <div>
-      <div className="flex items-center justify-between my-4 py-4 border-b">
-        <h1 className="text-xl font-semibold">Dashboard</h1>
-        <div className="space-x-4">
-          <NewWorkflowForm />
-          <Button variant="outline">
-            <Link href="/workflows">View All Workflows</Link>
-          </Button>
-        </div>
+    <ContentLayout title={"Dashboard"}>
+      <div className="p-4">
+        <h1 className="text-2xl font-semibold">Dashboard</h1>
+        <p className="text-gray-500">Welcome to the dashboard</p>
       </div>
-
-      <div className="space-y-4">
-        <PageSection sectionName="Important Metric">
-          <ExampleAreaChart />
-        </PageSection>
-      </div>
-    </div>
+    </ContentLayout>
   );
 };
 export default Page;
