a2b2615ed1dd471a2f02ae460962a70a685c2f3e
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-03-11 00:12:05 +0530

    Replace menu items with a new Settings option in menu list

--- lib/menu-list.ts
+++ lib/menu-list.ts
@@ -1,15 +1,4 @@
-import {
-  Anchor,
-  Box,
-  Group,
-  LayoutGrid,
-  LucideIcon,
-  NotebookTabs,
-  NotepadText,
-  Ship,
-  Users,
-  Users2,
-} from "lucide-react";
+import { Group, LayoutGrid, LucideIcon, Settings } from "lucide-react";
 
 type Submenu = {
   href: string;
@@ -42,45 +31,9 @@ export function getMenuList(): Group[] {
           submenus: [],
         },
         {
-          href: "/clients",
-          label: "Clients",
-          icon: Users2,
-          submenus: [],
-        },
-        {
-          href: "/enquiries",
-          label: "Enquiries",
-          icon: NotebookTabs,
-          submenus: [],
-        },
-        {
-          href: "/orders",
-          label: "Orders",
-          icon: Box,
-          submenus: [],
-        },
-        {
-          href: "/reports",
-          label: "Reports",
-          icon: NotepadText,
-          submenus: [],
-        },
-        {
-          href: "/ports-of-supply",
-          label: "Ports of Supply",
-          icon: Anchor,
-          submenus: [],
-        },
-        {
-          href: "/vessels",
-          label: "Vessels",
-          icon: Ship,
-          submenus: [],
-        },
-        {
-          href: "/traders",
-          label: "Traders",
-          icon: Users,
+          href: "/settings",
+          label: "Settings",
+          icon: Settings,
           submenus: [],
         },
       ],
